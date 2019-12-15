3852503274846484a60c7c3b2857a9b4acf5152b
Kevin Indra Mahendra <indramahendrakevin98@gmail.com>
2019-12-15 23:54:37 +0700

    Perbaikan Gameplay

---
+++ Assets/coinscript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class coinscript : MonoBehaviour
+{
+  	public int coinValue;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D other){
+	    if(other.tag == "Player"){
+      	AudioSource.PlayClipAtPoint
+        (this.GetComponent<AudioSource>().clip, this.transform.position);
+      	Destroy(this.gameObject);
+	    }
+  	}
+}
