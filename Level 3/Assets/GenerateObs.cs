7f281d2a071f0a6f78f4de07ec6250adc5b0d965
naufalrio <rionaufal@gmail.com>
2020-01-04 15:25:36 +0700

    Level 3
    Penambahan main scene level 3

---
+++ Level 3/Assets/GenerateObs.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateObs : MonoBehaviour
+{   
+    public GameObject jalan;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating ("CreateObstacle", 1f, 1f);
+    }
+    void CreateObstacle()
+    {
+        Instantiate (jalan);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
